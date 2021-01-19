55261a2f3541a52c276d720af30663b48f8fa142
trickered <brrumur476@gmail.com>
2021-01-18 20:56:36 -0600

    nuevo comentaio

--- Gestor.cpp
+++ Gestor.cpp
@@ -1,9 +1,13 @@
 //
 // Created by Brandon Rutch Murillo on 18/1/21.
 //
-
+#include "Lista.h"
+#include "Pila.h"
+#include "Cola.h"
 #include "Gestor.h"
 using namespace  std;
+
+//agregar una lista, pila y cola como atributos privados en el gestor.h para poder acceder a estas
 
 Gestor::Gestor() {}
 
